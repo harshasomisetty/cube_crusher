30dc4f75ee5b3a0304db1d4697efb044db1159dc
Harsha Somisetty <harshasomisetty7@gmail.com>
2024-02-07 21:23:48 -0500

    changed ports and app.js and listen

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+console.log('Starting app...');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +38,11 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+const port = process.env.PORT || 3000; // Use the port from the environment variable or default to 3000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app;
