3853f1886786eff9042c35522589f43cd4048d3e
Harsha Somisetty <harshasomisetty7@gmail.com>
2024-02-13 23:35:07 -0500

    changed endpoint

--- 3dgame/Assets/Scripts/Data/AppConfig.cs
+++ 3dgame/Assets/Scripts/Data/AppConfig.cs
@@ -3,6 +3,6 @@ namespace Data
     public static class AppConfig
     {
         // public static readonly string SERVER_ENDPOINT = "http://localhost:3001";
-        public static readonly string SERVER_ENDPOINT = "https://api.gameshift.dev";
+        public static readonly string SERVER_ENDPOINT = "https://api.cubecrusher.gameshift.dev";
     }
 }
