dac05f9e75f95c319d8c2424c715e681b6a23b75
Harsha Somisetty <harshasomisetty7@gmail.com>
2024-02-14 01:21:42 -0500

    cleanup

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -59,9 +59,7 @@ router.get('/:email/profile/gamesPlayed', async (req, res) => {
   const refId = email.split('@')[0];
 
   try {
-    console.log('about to check played');
     await checkUserExists(refId, email);
-    console.log('checked played');
     const profileNFT = await getUserProfileNFT(refId);
     res.json({
       email: email,
